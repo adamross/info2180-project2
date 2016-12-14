fe6f1e6403fbe83edca833c33c52b423fff303dd
Adam-Ross Findlator <adam-rossfindlator@Adam-Rosss-MacBook-Pro.local>
2016-12-14 16:13:17 -0500

    Commit3

--- fifteen.js
+++ fifteen.js
@@ -24,7 +24,25 @@ window.onload = function()
       if (moveCheck(parseInt(this.innerHTML))){
         swap(this.innerHTML-1);
         if (finished()===true){
-          setTimeout(function(){alert("You Won!");}, 500);
+          setTimeout(function()
+          {
+          document.getElementsByTagName("H1")[0].setAttribute("id", "change");
+          document.getElementById('change').innerHTML = ("YOU WON, CONGRATULATIONS");
+          }, 500);
+          setTimeout(function()
+          {
+          alert("The winning text will revert back to normal within ten seconds");
+        }, 1000); 
+
+          setTimeout(function()
+          {
+          document.getElementsByTagName("H1")[0].setAttribute("id", "change");
+          document.getElementById('change').innerHTML = ("Fifteen Puzzle");
+        }, 10000);
+
+
+
+
         }
         return;
       }
@@ -35,7 +53,8 @@ window.onload = function()
   posY = '300px';
 
   var shufflebutton = document.getElementById('shufflebutton');
-  shufflebutton.onclick = function(){
+  shufflebutton.onclick = function()
+  {
     for (var k = 0; k < 250; k++){
       var l = parseInt(Math.random()* 100) %4;
       if (l == 0){
